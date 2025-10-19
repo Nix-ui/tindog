c549e7a31642c0d25cee2e6931ee791c2a5f4a6e
CalebBav234 <calebmbaving@gmail.com>
2025-10-19 10:14:07 -0400

    feat: added jest files

---
+++ jest.setup.js
@@ -0,0 +1,15 @@
+global.localStorage = {
+  store: {},
+  getItem(key) {
+    return this.store[key] || null;
+  },
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  },
+  removeItem(key) {
+    delete this.store[key];
+  },
+  clear() {
+    this.store = {};
+  }
+};
