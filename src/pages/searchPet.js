71f96af61dc372ad85fb746183197093945d6788
CalebBav234 <calebmbaving@gmail.com>
2025-10-19 11:07:34 -0400

    feat: updated searchpet

--- src/pages/searchPet.js
+++ src/pages/searchPet.js
@@ -2,7 +2,10 @@ import PetCollectionInstance from '../model/PetCollectionInstance.js';
 import PetSearchPresenter from '../presenter/PetSearchPresenter.js';
 import PetSearchView from '../view/PetSearchView.js';
 
+export default function searchPetTemplate() {
+  const view = new PetSearchView();
+  const presenter = new PetSearchPresenter(PetCollectionInstance, view);
+  presenter.init();
 
-const view = new PetSearchView();
-const presenter = new PetSearchPresenter(PetCollectionInstance, view);
-presenter.init();
+  return view.getTemplate(); 
+}
