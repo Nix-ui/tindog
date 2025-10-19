1c806609196ef699d26500126db36067a7b0e94c
CalebBav234 <calebmbaving@gmail.com>
2025-10-19 09:49:44 -0400

    feat: updated petcollection model

--- src/model/PetCollectionModel.js
+++ src/model/PetCollectionModel.js
@@ -2,15 +2,43 @@ import PetModel from './PetModel.js';
 
 export default class PetCollectionModel {
   constructor() {
-    this.pets = [];
+    this.pets = JSON.parse(localStorage.getItem('pets')) || [];
   }
 
-  addPet(pet) {
-    if (pet instanceof PetModel) {
-      this.pets.push(pet);
+  /**
+   * Guarda en localStorage
+   */
+  _save() {
+    localStorage.setItem('pets', JSON.stringify(this.pets));
+  }
+
+  /**
+   * Agrega una mascota nueva
+   * @param {object|PetModel} petData
+   * @returns {PetModel}
+   */
+  addPet(petData) {
+    let newPet;
+    if (petData instanceof PetModel) {
+      newPet = petData;
     } else {
-      throw new Error('Debe ser una instancia de PetModel');
+      newPet = new PetModel(
+        this.pets.length + 1,
+        petData.name,
+        petData.address || '',
+        false,
+        petData.age,
+        petData.breed,
+        petData.size || '',
+        petData.description || '',
+        petData.owner || '',
+        petData.image || ''
+      );
     }
+
+    this.pets.push(newPet);
+    this._save();
+    return newPet;
   }
 
   getAllPets() {
@@ -24,5 +52,10 @@ export default class PetCollectionModel {
   filterByBreed(breed) {
     if (!breed) return this.getAllPets();
     return this.pets.filter(p => p.breed.toLowerCase() === breed.toLowerCase());
+  }
+
+  clearAll() {
+    this.pets = [];
+    this._save();
   }
 }
