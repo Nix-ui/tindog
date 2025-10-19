52d7e0c479deeedea4c099b08aad0ae9e8f2be52
CalebBav234 <calebmbaving@gmail.com>
2025-10-19 12:11:44 -0400

    feat: updated various files

--- src/presenter/PetSearchPresenter.js
+++ src/presenter/PetSearchPresenter.js
@@ -2,15 +2,18 @@ export default class PetSearchPresenter {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.handleBreedChange = this.handleBreedChange.bind(this);
   }
 
   init() {
     const breeds = this.model.getAllBreeds();
     this.view.populateBreedOptions(breeds);
+    this.view.removeBreedChangeListener?.(this.handleBreedChange);
+    this.view.onBreedChange(this.handleBreedChange);
+  }
 
-    this.view.onBreedChange(breed => {
-      const pets = this.model.filterByBreed(breed);
-      this.view.showPets(pets);
-    });
+  handleBreedChange(breed) {
+    const pets = this.model.filterByBreed(breed);
+    this.view.showPets(pets);
   }
 }
