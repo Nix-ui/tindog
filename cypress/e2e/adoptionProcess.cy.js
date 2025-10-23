4b84a8a81270dce0f2b5a49a2016856c3a8dd5b9
ERICK1766 <guzmanquispeerickivan@gmail.com>
2025-10-23 01:34:20 -0400

    test(e2e): evitar iniciar adopción si la mascota ya está en proceso

--- cypress/e2e/adoptionProcess.cy.js
+++ cypress/e2e/adoptionProcess.cy.js
@@ -3,4 +3,15 @@ describe('Proceso de Adopción', () => {
     cy.visit('/');
     cy.get('[data-action="contact"]').should('exist');
   });
+
+  it('No debería permitir iniciar adopción si la mascota ya está en proceso', () => {
+    cy.visit('/');
+    cy.get('[data-action="contact"]').first().click();
+    cy.get('[data-action="contact"]').first().click();
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.include('ya está en proceso');
+    });
+  });
+  
 });
