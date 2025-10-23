74c983b8544f354846d3948d54f70fa60c30617f
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-10-23 00:04:54 -0400

    La solicitud cuenta con un callback que filtrara los datos

--- src/models/filter/FilterRequest.spec.js
+++ src/models/filter/FilterRequest.spec.js
@@ -5,4 +5,12 @@ describe("Solicitud de Filtro", () => {
         const filter = new FilterRequest();
         expect(filter).toBeInstanceOf(FilterRequest);
     });
+    it('Deberia tener un callback', () => {
+        const filter = new FilterRequest('','','',()=>{return 'callback'});
+        expect(filter.callback).toBeInstanceOf(Function);
+        const response = filter.callback();
+        expect(response).toBe('callback');
+        const filter2 = new FilterRequest('','','','');
+        expect(filter2.callback).toBeInstanceOf(Function);
+    });
 });
