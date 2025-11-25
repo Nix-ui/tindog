d0f63f6d740060436a056e7d7f07ef9ea769e491
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-11-25 09:55:46 -0400

    fix:Las mascotas ahora se muestran bien en todas las vistas y se mantiene consistencia

--- src/repository/PetRepository.js
+++ src/repository/PetRepository.js
@@ -1,22 +1,34 @@
 import petData from '../../data/pets.json';
 import PetModel from '../models/pet/petModel';
 import FilterRequest from '../models/filter/FilterRequest';
+import LocalRepository from './LocalRepository';
 
 export default class PetRepository {
     constructor() {
+        this.LocalRepository = new LocalRepository();
         this.pets = petData.map(pet => new PetModel(pet.id, pet.name, pet.address, pet.isLiked, pet.age, pet.breed, pet.size, pet.description, pet.owner, pet.image));
         this.breeds = [...new Set(petData.map(pet => pet.breed))];
     }
     getAllPets() {
+        this.#checkIfPetsInLocalStorage();
         return this.pets;
     }
+    #checkIfPetsInLocalStorage() {
+        if(this.LocalRepository.existsInLocalStorage('pets')){
+            this.pets = this.LocalRepository.getPetsFromLocalStorage();
+        }
+    }
     registerPet(pet) {
+        this.#checkIfPetsInLocalStorage();
         this.pets.push(pet);
+        this.LocalRepository.saveInLocalStorage('pets', this.pets);
     }
     filterByBreed(breed) {
+        this.#checkIfPetsInLocalStorage();
         return this.pets.filter(pet => pet.breed === breed);
     }
     filterByAddress(address) {
+        this.#checkIfPetsInLocalStorage();
         return this.pets.filter(pet => pet.address.includes(address));
     }
     getBreeds() {
@@ -28,6 +40,7 @@ export default class PetRepository {
      * @returns 
      */
     filterBy(filters){
+        this.#checkIfPetsInLocalStorage();
         let filteredPets = this.pets;
         filters.forEach(filter=>{
             filteredPets = filteredPets.filter(pet => {
@@ -36,4 +49,5 @@ export default class PetRepository {
         })
         return filteredPets;
     }
+
 }
