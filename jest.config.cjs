b4a4ff48b2108d78318c0908c82fafdb02ee329f
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-11-27 15:43:29 -0400

    fix: jest exclude backend and node_modules

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,9 @@
 module.exports = {
   setupFiles: ['./jest.setup.js'],
+  testMatch: [
+    "**/src/**/*.spec.js",
+    "**/src/**/*.test.js"
+  ],
   testPathIgnorePatterns: [
     "/node_modules/",
     "/dist/",
