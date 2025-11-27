b682de6b613159bc34a8f9dc4c9836b43c74f5ef
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-11-26 23:08:06 -0400

    feat:Integracion con el backend y haciendo persistencia de datos

---
+++ src/service/user.service.js
@@ -0,0 +1,26 @@
+import config from "../config/config";
+import { RegisterUserModel } from "../models/user/register-user.model";
+
+export default class UserService {
+    
+    constructor() {
+        if(UserService.instance) {
+            return UserService.instance;
+        }
+        UserService.instance = this;
+    }
+    /**
+     * 
+     * @param {RegisterUserModel} user 
+     */
+    async registerUser(user) {
+        const response = await fetch(`${config.API_URL}/users`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+        return response.json();
+    }
+}
